efda18b73df1788508160511dc56585cc5cf3224
Raamprasaadh <ramprasadhsurya@gmail.com>
2020-09-27 22:53:35 +0530

    adds router and required pages

---
+++ client/src/navBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function NavBar() {
+    return (
+        <div>
+            <h4>Title-logo</h4>
+            <ul>
+                <li>Produts</li>
+                <li>Cart</li>
+            </ul>
+            
+        </div>
+    )
+}
